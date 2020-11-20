6b9b848afe8c2c1f0c912b1909f5a99d5f82c83e
Igor Balbino <66763979+igorbalbino@users.noreply.github.com>
2020-11-19 21:32:37 -0300

    Primeiro Script
    Primeiro script feito em php.
    testando o comando echo e criando variaveis.

---
+++ first.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Título da página</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        
+        <div>
+            <?php
+
+                $nome=(string)"Jose das Couves";
+                $ideda=(int)37;
+                $peso=84.4;
+
+                echo 'Olá mundo!';
+            ?>
+        </div>
+
+    </body>
+</html>
